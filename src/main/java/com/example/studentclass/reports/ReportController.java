7c08a7f80d8fecb20f38f3e2c04783b80dd4b810
VasilevID1111 <VasilevID1111@mail.ru>
2023-05-18 06:54:14 +0500

    Добавлен формировать excel

--- src/main/java/com/example/studentclass/reports/ReportController.java
+++ src/main/java/com/example/studentclass/reports/ReportController.java
@@ -1,18 +1,29 @@
 package com.example.studentclass.reports;
 
 import com.example.studentclass.computers.ComputerService;
+import com.example.studentclass.shedule.SheduleDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,10 +33,14 @@ public class ReportController {
     @Autowired
     private final ReportService reportService;
     private final ComputerService computerService;
+    private List<SheduleDTO> globalVisits = new ArrayList<>();
     @GetMapping("/report")
-    public String report(Model model) {
+    public String report(Model model,
+                         RedirectAttributes redirectAttributes) {
         model.addAttribute("computers", computerService.getComputersAllDesc());
-        model.addAttribute("visits",reportService.getOneMonthShedule());
+        List<SheduleDTO> visits = reportService.getOneMonthShedule();
+        globalVisits = visits;
+        model.addAttribute("visits",visits);
         return "report";
     }
     @PostMapping("/report")
@@ -33,9 +48,35 @@ public class ReportController {
                             @RequestParam("compId") Integer compId,
                             @RequestParam("dateFrom") String dateFrom,
                             @RequestParam("dateTo") String dateTo,
-                            Model model) throws ParseException {
+                            Model model,
+                            RedirectAttributes redirectAttributes) throws ParseException {
         model.addAttribute("computers", computerService.getComputersAllDesc());
-        model.addAttribute("visits",reportService.getSheduleWithSettings(login,compId,dateFrom,dateTo));
+        List<SheduleDTO> visits = reportService.getSheduleWithSettings(login,compId,dateFrom,dateTo);
+        globalVisits = visits;
+        model.addAttribute("visits",visits);
         return "report";
+    }
+    @PostMapping("/report/xlsx")
+    public ResponseEntity<byte[]> getXlsx(RedirectAttributes redirectAttributes,
+                                          Model model)
+            throws IOException {
+        @SuppressWarnings("unchecked")
+        List<SheduleDTO> visits = globalVisits;
+        Workbook workbook = reportService.createXslxFile(visits);
+        // Convert workbook to byte array
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        workbook.write(byteArrayOutputStream);
+        byte[] workbookBytes = byteArrayOutputStream.toByteArray();
+
+        // Set response headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        headers.setContentDispositionFormData("attachment", "report.xlsx");
+
+        // Send workbook as a file download
+        return ResponseEntity
+                .ok()
+                .headers(headers)
+                .body(workbookBytes);
     }
 }
--- src/main/java/com/example/studentclass/reports/ReportService.java
+++ src/main/java/com/example/studentclass/reports/ReportService.java
@@ -2,15 +2,21 @@ package com.example.studentclass.reports;
 
 import com.example.studentclass.shedule.SheduleDAO;
 import com.example.studentclass.shedule.SheduleDTO;
-import com.example.studentclass.shedule.SheduleService;
+import com.example.studentclass.users.UserDTO;
 import lombok.AllArgsConstructor;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class ReportService {
@@ -26,12 +32,71 @@ public class ReportService {
         // Subtract two weeks from the current date
         Date twoWeeksEarlier = new Date(currentDate.getTime() - twoWeeksInMillis);
         Date twoWeeksLater = new Date(currentDate.getTime() + twoWeeksInMillis);
-        return sheduleDAO.findAllBetweenTwoDate(twoWeeksEarlier,twoWeeksLater);
+        return sheduleDAO.findAllBetweenTwoDate(twoWeeksEarlier, twoWeeksLater);
     }
 
     public List<SheduleDTO> getSheduleWithSettings(String login, Integer compId, String dateFrom, String dateTo) throws ParseException {
         Date dateFromParsed = (dateFrom.isEmpty() || dateFrom == null) ? null : dateFormat.parse(dateFrom);
         Date dateToParsed = (dateTo.isEmpty() || dateTo == null) ? null : dateFormat.parse(dateTo);
-        return sheduleDAO.findSheduleDTOSBySettings("%"+login+"%",compId,dateFromParsed,dateToParsed);
+        return sheduleDAO.findSheduleDTOSBySettings("%" + login + "%", compId, dateFromParsed, dateToParsed);
+    }
+
+    public Workbook createXslxFile(List<SheduleDTO> visits) {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Отчет по студентам");
+
+        // Create headers
+        Row headerRow = sheet.createRow(0);
+        headerRow.createCell(0).setCellValue("Активность");
+        headerRow.createCell(1).setCellValue("Логин");
+        headerRow.createCell(2).setCellValue("ФИО");
+        headerRow.createCell(3).setCellValue("Почта");
+        headerRow.createCell(4).setCellValue("Время");
+        headerRow.createCell(5).setCellValue("Дата");
+        headerRow.createCell(6).setCellValue("Id компьютера");
+        headerRow.createCell(7).setCellValue("Имя компьютера");
+        headerRow.createCell(8).setCellValue("Статус");
+
+        // Populate data rows
+        int rowNum = 1;
+        for (SheduleDTO visit : visits) {
+            Row row = sheet.createRow(rowNum++);
+            UserDTO user = visit.getUser();
+            row.createCell(0).setCellValue(user.isActive());
+            row.createCell(1).setCellValue(user.getLogin());
+            row.createCell(2).setCellValue(user.getFio());
+            row.createCell(3).setCellValue(user.getEmail());
+            row.createCell(4).setCellValue(convertTimeRange(visit.getTimed_blocks()));
+            row.createCell(5).setCellValue(visit.getDate().toString());
+            row.createCell(6).setCellValue(visit.getComputer().getCompId());
+            row.createCell(7).setCellValue(visit.getComputer().getName());
+            row.createCell(8).setCellValue(visit.getStatus());
+        }
+
+        // Auto-size columns
+        for (int i = 0; i < sheet.getRow(0).getLastCellNum(); i++) {
+            sheet.autoSizeColumn(i);
+        }
+        return workbook;
+    }
+
+    public String convertTimeRange(String timeRange) {
+        int startHour = 9;
+        int startMinute = 0;
+        int timeSlotMinutes = 15;
+
+        String[] timeParts = timeRange.split("-");
+        int start = Integer.parseInt(timeParts[0]);
+        int end = Integer.parseInt(timeParts[1]);
+
+        int startTime = startHour * 60 + startMinute + start * timeSlotMinutes;
+        int endTime = startHour * 60 + startMinute + end * timeSlotMinutes + 15;
+
+        String startHourStr = String.format("%02d", startTime / 60);
+        String startMinuteStr = String.format("%02d", startTime % 60);
+        String endHourStr = String.format("%02d", endTime / 60);
+        String endMinuteStr = String.format("%02d", endTime % 60);
+
+        return "c " + startHourStr + "-" + startMinuteStr + " до " + endHourStr + "-" + endMinuteStr;
     }
 }
